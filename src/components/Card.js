10b1c0c142e9b8d57088fbd70ebb001ce753e8e6
SzewczykMichal <13821@eppuz.edu.pl>
2024-06-10 22:21:32 +0200

    test github pages 10.06.2024

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 function Card({item, id, handleClick}){
     const itemClass = item.stat ? " active " + item.stat : ""
-    //Showing card
+
     return (
     <div className={"card" + itemClass} onClick={() => handleClick(id)}>
         <div className="emote">{item.text}</div>
